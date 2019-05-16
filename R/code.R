d3b6feb3e4a91abe08dec98b0e5a2c5a58f5eceb
Martí Barri <martibarri23@gmail.com>
2019-05-16 21:45:28 +0200

    functional building + bug fixes

--- R/code.R
+++ R/code.R
@@ -1,7 +1,7 @@
 #' Title
 #'
-#' @return
-#' @export
+#' @details Download file from 'download.url' and save it as 'destination.file' in dir.data
+#' Path is relative to working directory
 #'
 #' @examples
 create.dir.data <- function() {
@@ -19,6 +19,8 @@ create.dir.data <- function() {
 #' @param download.url
 #' @param destination.file
 #'
+#' @export
+#'
 #' @examples
 download.data <- function(url, destfile) {
   create.dir.data()
@@ -28,9 +30,17 @@ download.data <- function(url, destfile) {
   }
 }
 
-get.opendata.file <- function(url = NULL) {
+#' Title
+#'
+#' @param url
+#'
+#' @return df.tcp21
+#' @export
+#'
+#' @examples
+get.opendata <- function(opendata.url = NULL) {
   # Obtain raw data of opendata.rapid7.com website
-  if (is.null(url)) { # Default file
+  if (is.null(opendata.url)) { # Default file
     opendata.url <- "https://opendata.rapid7.com/sonar.tcp/2019-04-04-1554350684-ftp_21.csv.gz"
   }
   # split URL by '/' and get last value (name on file)
@@ -41,20 +51,27 @@ get.opendata.file <- function(url = NULL) {
     download.data(url = opendata.url, destfile = opendata.file)
     R.utils::gunzip(opendata.file.gz)
   }
-  df.tcp21 <- read.csv(opendata.file, stringsAsFactors = FALSE)
+  df.tcp21 <- read.csv(file.path(getwd(), "data", opendata.file), stringsAsFactors = FALSE)
   return(df.tcp21)
 }
 
-get.mixmind.file <- function() {
+#' Title
+#'
+#' @return df.maxmind
+#' @export
+#'
+#' @examples
+get.mixmind <- function() {
   # Maxmind - Obtain raw data (city)
   maxmind.url <- "https://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip"
   maxmind.file <- "maxmind.zip"
   download.data(url = maxmind.url, destfile = maxmind.file)
-  zipfiles <- unzip(zipfile = maxmind.file, list = T)
+  zipfiles <- unzip(zipfile = file.path(getwd(), "data", maxmind.file), list = T)
   maxmind.source <- zipfiles$Name[grep(pattern = ".*GeoLite2-City-Blocks-IPv4.csv", x = zipfiles$Name)]
-  unzip(zipfile = maxmind.file, exdir = file.path(getwd(), "data"), files = maxmind.source)
+  unzip(zipfile = file.path(getwd(), "data", maxmind.file),
+        exdir = file.path(getwd(), "data"),
+        files = maxmind.source)
   maxmind.source <- file.path(getwd(), "data", maxmind.source)
-  rm(maxmind.file, zipfiles)
   df.maxmind <- read.csv(maxmind.source, stringsAsFactors = FALSE)
   return(df.maxmind)
 }
