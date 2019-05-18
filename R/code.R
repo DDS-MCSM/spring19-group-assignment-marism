f66b88722b0486f9d784e64224d6802151252a87
Martí Barri <martibarri23@gmail.com>
2019-05-18 20:09:51 +0200

    roxygen2 correct

--- R/code.R
+++ R/code.R
@@ -1,25 +1,25 @@
 #' @title Imprirmir por consola
 #' @description Funcion que imprime por pantalla, simpre y cuando este habilitada a true la variable verbose
 #' @details No se le pasa ningun valor a la función ya que dejamos como predefinido "data" por defecto, sin capacidad de ser alterado.
-#' 
+#'
 #' @param x Texto que queremos imprimir
-#' 
+#'
 #' @export
 #' @examples
 #' syso("Hello World!!")
 syso <- function(x) {
   verbose <- TRUE
-if (verbose) print(x)
+  if (verbose) print(x)
 }
 
 
 #' @title Valida la existencia del directorio de trabajo "data"
 #' @description Función que establece el directorio de trabajo en data y en caso de no existir lo crea.
 #' @details No se le pasa ningun valor a la función ya que dejamos como predefinido "data" por defecto, sin capacidad de ser alterado.
-#' 
+#'
 #' @param
-#' 
 #' @export
+#'
 #' @examples
 #' create.dir.data()
 create.dir.data <- function() {
@@ -30,14 +30,14 @@ create.dir.data <- function() {
 }
 
 #' @title Descarga de fichero de internet
-#' @description Función que descarga un fichero de una url y lo guarda con el nombre del fichero que indiquemos. 
+#' @description Función que descarga un fichero de una url y lo guarda con el nombre del fichero que indiquemos.
 #' @details Descarga de la '$url' y lo guarda con el nombre de '$destfile', internamente hace una llamada a 'create.dir.data()',
 #' para asegurarnos que el directorio de trabajo data existe, por otro lado valida que el nombre del fichero ya no este en el repositorio
 #' de trabajo y solo en ese caso se lo descarga.
-#' 
+#'
 #' @param url Lugar donde iremos a buscar los datos
 #' @param destfile Nombre del archivo de destino con el que guardaremos los datos
-#' 
+#'
 #' @examples
 #' download.data(url, destfile)
 download.data <- function(url, destfile) {
@@ -48,13 +48,13 @@ download.data <- function(url, destfile) {
   }
 }
 
-#' @title Obtención fichero opendata 
-#' @description Función que se conecta a la web de rapit7 que lleva implicita, y se descarga un fichero en formato gz, el cual posteriormente 
-#' lo descomprime y nos lo retorna en formato csv, borrando el fichero comprimido. En esta funcion se utlizan otras funciones ya descritas con anterioridad, por ejemplo download.data 
-#' @details Funcion que nos retorna un fichero con las IP que tienen abierto el puerto TCP 
-#' 
-#' 
-#' @return df.tcp21 
+#' @title Obtención fichero opendata
+#' @description Función que se conecta a la web de rapit7 que lleva implicita, y se descarga un fichero en formato gz, el cual posteriormente
+#' lo descomprime y nos lo retorna en formato csv, borrando el fichero comprimido. En esta funcion se utlizan otras funciones ya descritas con anterioridad, por ejemplo download.data
+#' @details Funcion que nos retorna un fichero con las IP que tienen abierto el puerto TCP
+#'
+#'
+#' @return df.tcp21
 #' @export
 #'
 #' @examples
@@ -74,8 +74,8 @@ get.opendata <- function() {
 }
 
 #' @title Obtención del fichero maxmid
-#' @description Función que se conecta a la web de geolite.maxmind que lleva implicita, y se descarga un fichero .zip, 
-#' de la lista de ficheros unicamente se queda con el que cumple la expresion regular *GeoLite2-City-Blocks-IPv4.csv 
+#' @description Función que se conecta a la web de geolite.maxmind que lleva implicita, y se descarga un fichero .zip,
+#' de la lista de ficheros unicamente se queda con el que cumple la expresion regular *GeoLite2-City-Blocks-IPv4.csv
 #' lo descomprime y nos lo retona como csv
 #' @details Funcion que nos retorna un fichero con la geolicalizacion de IPV4 por ciudad
 #' @return df.maxmind
@@ -100,7 +100,7 @@ get.maxmind <- function() {
 
 #' @title Función para convertir las IP en numeros
 #' @description Dada una IP te la convierte a numero quitando los espacios.
-#' @details Le pasas el datframe y la columna donde se encuentran las IP 
+#' @details Le pasas el datframe y la columna donde se encuentran las IP
 #' y el te retorna el mismo data frame convertiendo las IP en nuemeros
 #'
 #' @param df Fichero que contiene los data frame
@@ -121,7 +121,7 @@ add.numeric.ip <- function(df, ip.col) {
 
 #' @title Funció para obtener las n primeras muestras de un data frame
 #' @description Función a la que le pasas un data frame con muchos datos y te retorna las n linias que le pases
-#' como parametro
+#'como parametro
 #' @details Necesitas pasarle un df y un valor numerico
 #' @param df Fichero que contiene el data frame
 #' @param nrows Numero de muestras que queremos tratar
@@ -137,11 +137,11 @@ generate.df <- function(df, nrows) {
   return(partial.df)
 }
 
-#' @title Identificacion del rango de la IP utlizando threads paralelos 
-#' @description Función que permite optimizar al maximo la capacidad de procesado del ordenador, 
-#' enviando procesos por cada uno de los cores que tiene disponible el ordenador exceptuando de uno 
+#' @title Identificacion del rango de la IP utlizando threads paralelos
+#' @description Función que permite optimizar al maximo la capacidad de procesado del ordenador,
+#' enviando procesos por cada uno de los cores que tiene disponible el ordenador exceptuando de uno
 #' que lo deja para procesos propios de la maquina.
-#' @details Por cada IP, miramos a que rango de IP pertenece, ya en el df.maxmind, fichero que nos 
+#' @details Por cada IP, miramos a que rango de IP pertenece, ya en el df.maxmind, fichero que nos
 #' indica la geolocalizacion, nos lo especifican los rangos de IP con mnascaras de red equivalente. Nos retorna el df
 #'
 #' @param df.maxmind Data frame de maxmind
@@ -171,7 +171,7 @@ geolocate.ip.range <- function(df.maxmind, df.scans) {
 }
 
 #' @title Unió de resultados
-#' @description Función que nos une los dos data frame, para optener los resultados que queremos obtener 
+#' @description Función que nos une los dos data frame, para optener los resultados que queremos obtener
 #' La geolocalización a nivel de ciudad de las IP identifcadas con vulnerabilidad.
 #' @details Dados dos df de entrada, retorna uno con los resultados que queremos mostrar
 #' @param df.maxmind Data frame de maxmind
@@ -200,7 +200,7 @@ join.tidy.data <- function(df.maxmind, df.scans) {
 }
 
 #' @title Ejecución de CodeBook
-#' @description Función que inicializa una serie de valirables para simular el comportamiento definido en 
+#' @description Función que inicializa una serie de valirables para simular el comportamiento definido en
 #' el codigo facilitado en la práctica.
 #' @details Dato unas variables llamamos a lo que sería el equivalente a nuestro main, el corzando del progrma
 #'
@@ -211,19 +211,18 @@ join.tidy.data <- function(df.maxmind, df.scans) {
 #' codebook.ftp()
 codebook.ftp <- function() {
   tini <- Sys.time()
+  url <- NULL
   sample <- 500
   output.name <- "geoftps.rds"
-  codebook(sample, output.name)
+  codebook(url, sample, output.name)
   fini <- Sys.time()
   fini - tini
 }
 
 #' @title Funcion general
-#' @description Función que ejecuta ordenadamente todas las funciones decladaras, y simula el comportamiento 
+#' @description Función que ejecuta ordenadamente todas las funciones decladaras, y simula el comportamiento
 #' del codigo facilitado en la práctica.
-#' 
-#' @details 
-#' 
+#'
 #' @param sample Numero de muestras.
 #' @param output.name Nombre del fichero de salida donde guardaremos los datos.
 #'
@@ -232,18 +231,18 @@ codebook.ftp <- function() {
 #'
 #' @examples
 #' codebook(sample, output.name)
-codebook <- function(sample, output.name) {
-  
+codebook <- function(url, sample, output.name) {
+
   syso("Inicio de la ejecución....")
   syso("...")
   syso("Inicio de la obtención fichero opendata...")
-  df.tcp <- get.opendata() # Default value: tcp21
+  df.tcp <- get.opendata(url) # Default value: tcp21
   syso("Fin de la optencion fichero opendata")
-  
+
   syso("Inicio de la obtención del fichero maxmind...")
   df.maxmind <- get.maxmind()
   syso("Fin obtención del fichero maxmind.")
-  
+
   df.tcp <- add.numeric.ip(df.tcp, "saddr")
   df.tcp <- add.numeric.ip(df.tcp, "daddr")
 
@@ -267,7 +266,7 @@ codebook <- function(sample, output.name) {
   df$is_anonymous_proxy <- as.factor(df$is_anonymous_proxy)
   df$is_satellite_provider <- as.factor(df$is_satellite_provider)
   saveRDS(object = df, file = file.path(getwd(), "data", output.name))
-  #syso(c("Los resuldos los podra ver en la carpeta data ",file)) 
+  #syso(c("Los resuldos los podra ver en la carpeta data ",file))
 }
 
 
